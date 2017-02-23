315e203b7f10fbe9edf6ad7e7124fda3884f5395
Jan Fajerski <jfajerski@suse.com>
2017-02-23 14:45:20 +0100

    deepsea: add policy.cfg
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>

--- qa/tasks/deepsea.py
+++ qa/tasks/deepsea.py
@@ -2,6 +2,7 @@
 Task to deploy clusters with DeepSea
 '''
 import logging
+from pprint import pprint
 
 from teuthology.config import config as teuth_config
 from teuthology.exceptions import CommandFailedError
@@ -48,12 +49,19 @@ class DeepSea(Task):
             run.Raw(';'),
             'sudo',
             'make',
-            'install'
+            'install',
+            run.Raw(';'),
+            'sudo',
+            'chown',
+            '-R',
+            'salt',
+            '/srv/pillar/ceph/'
             ])
 
         self.salt.init_minions()
         self.salt.start_master()
         self.salt.start_minions()
+        self.salt.ping_minions()
 
     def begin(self):
         super(DeepSea, self).begin()
@@ -63,8 +71,14 @@ class DeepSea(Task):
         super(DeepSea, self).end()
 
     def test_stage_1_to_3(self):
-        self._emulate_stage_0()
+        self.__emulate_stage_0()
         self.__stage1()
+        self.__map_roles_to_policy_cfg()
+        self.master_remote.run(args = [
+            'sudo',
+            'cat',
+            '/srv/pillar/ceph/proposals/policy.cfg'
+            ])
         # self.__stage2()
         # self.__stage3()
         # self.__is_cluster_healthy()
@@ -74,14 +88,45 @@ class DeepSea(Task):
         stage 0 might reboot nodes. To avoid this for now lets emulate most parts of
         it
         '''
+        # TODO target only G@job_id: $job_id
         self.salt.master_remote.run(args = [
-            'sudo', 'salt', '*', 'ceph.sync',
-            'sudo', 'salt', '*', 'ceph.mines',
-            'sudo', 'salt', '*', 'ceph.packages.common',
+            'sudo', 'salt', '*', 'state.apply', 'ceph.sync',
+            run.Raw(';'),
+            'sudo', 'salt', '*', 'state.apply', 'ceph.mines',
+            run.Raw(';'),
+            'sudo', 'salt', '*', 'state.apply', 'ceph.packages.common',
             ])
 
     def __stage1(self):
         self.salt.master_remote.run(args = [
             'sudo', 'salt-run', 'state.orch', 'ceph.stage.1'])
+
+    def __stage2(self):
+        self.salt.master_remote.run(args = [
+            'sudo', 'salt-run', 'state.orch', 'ceph.stage.2'])
+
+    def __map_roles_to_policy_cfg(self):
+        # TODO this should probably happen in a random tmp dir...look in misc
+        misc.sh('echo "cluster-ceph/cluster/*.sls\
+                config/stack/default/global.yml\
+                config/stack/default/ceph/cluster.yml" > /tmp/policy.cfg'
+                )
+        for _remote, roles_for_host in self.ctx.cluster.remotes.iteritems():
+            for role in roles_for_host:
+                nodename = str(str(_remote).split('.')[0]).split('@')[1]
+                if(role.startswith('osd')):
+                    misc.sh('echo "profile-*-1/cluster/{}.sls" >> /tmp/policy.cfg'.format(nodename))
+                    misc.sh('echo "profile-*-1/stack/default/ceph/minions/*.yml{}.sls" >> /tmp/policy.cfg'.format(nodename))
+                if(role.startswith('mon')):
+                    misc.sh('echo "role-admin/cluster/{}.sls" >> /tmp/policy.cfg'.format(nodename))
+                    misc.sh('role-mon/cluster/{}.sls'.format(nodename))
+                    misc.sh('echo "role-mon/stack/default/ceph/minions/{}.sls" >> /tmp/policy.cfg'.format(nodename))
+        misc.sh('scp /tmp/policy.cfg {}:'.format(self.master_remote.name))
+        self.salt.master_remote.run(args = [
+            'sudo',
+            'mv',
+            'policy.cfg'
+            '/srv/pillar/ceph/proposals/policy.cfg'
+            ])
 
 task = DeepSea
