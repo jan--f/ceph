fb8487e0f7ae62ca2c1b9625d8097e698afd34fa
Jan Fajerski <jfajerski@suse.com>
2017-08-04 10:23:11 +0200

    pybind/mgr/prometheus: prefix metrics with 'ceph'; replace :: with _
    Both follow prometheus best practices. While : is a legal metric
    character, "Exposed metrics should not contain colons, these are for
    users to use when aggregating."
    
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>

--- src/pybind/mgr/prometheus/module.py
+++ src/pybind/mgr/prometheus/module.py
@@ -76,7 +76,16 @@ class Metric(object):
 
         def promethize(path):
             ''' replace illegal metric name characters '''
-            return path.replace('.', '_').replace('-', '_')
+            result = path.replace('.', '_').replace('+', '_plus').replace('::', '_')
+
+            # Hyphens usually turn into underscores, unless they are
+            # trailing
+            if result.endswith("-"):
+                result = result[0:-1] + "_minus"
+            else:
+                result = result.replace("-", "_")
+
+            return "ceph_{0}".format(result)
 
         def floatstr(value):
             ''' represent as Go-compatible float '''
